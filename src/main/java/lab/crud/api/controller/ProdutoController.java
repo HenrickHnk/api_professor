dea2cca9eb34e84e9a623b0e3b84c21678014230
Henrick Pereira Ferreira <henrickhrk0903@gmail.com>
2025-09-12 22:14:21 -0300

    Add Produto

--- src/main/java/lab/crud/api/controller/ProdutoController.java
+++ src/main/java/lab/crud/api/controller/ProdutoController.java
@@ -1,9 +1,19 @@
 package lab.crud.api.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lab.crud.api.model.Produto;
 
 @RestController
 public class ProdutoController {
 
+//	@RequestMapping("method = RequestMethod.POST, path = "/produto")
+	@PostMapping("/produto")
+	public ResponseEntity<Produto> novo(Produto produto) {
+		return null;
+	}
 	
 }
